77329eec79bf73d08e51c7ebebd23e70bc7fe2b3
Sourav Rungta <67090089+souravrungta@users.noreply.github.com>
2022-04-15 23:09:32 +0530

    Delete prog1.java

--- prog1.java
+++
@@ -1,8 +0,0 @@
-//Name: Sourav Kumar Rungta
-//Problem: Program for HelloWorld.
-
-class prog1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!"); 
-    }
-}
