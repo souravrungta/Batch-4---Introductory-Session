c67fdb8df909e74ae59bb3fd656146036e98d462
Sourav Rungta <67090089+souravrungta@users.noreply.github.com>
2022-04-15 22:55:47 +0530

    Add files via upload

---
+++ prog2.java
@@ -0,0 +1,10 @@
+//Name: Sourav Kumar Rungta
+//Problem: Program for the area of square.
+
+class prog2 {
+    public static void main(String[] args) {
+        int side=5;
+        int area=side*side;
+        System.out.println("Area of square :" + area); 
+    }
+}
